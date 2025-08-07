f88cb166485b273346023133094383e9d82b4c6e
root-server <hatekriss@gmail.com>
2025-08-07 18:01:26 +0300

    Add tests for fractional mu and sigma

--- pert.test.js
+++ pert.test.js
@@ -11,3 +11,8 @@ test('mu and sigma for values 2,5,14', () => {
   assert.strictEqual(mu(2, 5, 14), 6);
   assert.strictEqual(sigma(2, 5, 14), 2);
 });
+
+test('mu and sigma for fractional values 1.5,2.5,4.5', () => {
+  assert.strictEqual(mu(1.5, 2.5, 4.5), 8 / 3);
+  assert.strictEqual(sigma(1.5, 2.5, 4.5), 0.5);
+});
